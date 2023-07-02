ea8ff8e2058d61a2b94fc722e0533fe3891f7c53
Gustavo Simonelli Franco <g19sfranco@gmail.com>
2023-07-02 14:05:29 -0300

    Update ex054.py

--- EXERCISES(PT-BR)/ex054.py
+++ EXERCISES(PT-BR)/ex054.py
@@ -1,3 +1,11 @@
 from datetime import date
 x = date.today().year
-print(x)
+menor = 0
+maior = 0
+for j in range(0,7):
+    idade = int(input('Em que ano a {}ª pessoa nasceu? '))
+    if x - idade < 18:
+        menor += 1
+    else:
+        maior += 1
+print('Ao todo tivemos {} pessoa(s) maior(s)\nde idade e {} menor(s) de idade.'.format(maior, menor))
