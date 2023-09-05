ea7336b88c60d707a8f1c52db5249de87c149c74
Gustavo Simonelli Franco <g19sfranco@gmail.com>
2023-09-04 21:56:50 -0300

    Create ex068.py

---
+++ EXERCISES(PT-BR)/ex068.py
@@ -0,0 +1,18 @@
+from random import randint
+gam_Win = 0
+gam_Ask = 'YES'
+while gam_Ask.upper() in 'YES':
+    num_Ask = int(input('Which number? '))
+    num_Npc = randint(1, 10)
+    num_End = num_Ask+num_Npc
+    opt_Ask = str(input('Pair or  odd? ')).strip().upper()
+    sum_End = 'Pair' if (num_End%2)==0 else 'Odd'
+    print(f'The sum of your {num_Ask} plus bot {num_Npc} is {num_End}. {sum_End}!', end='')
+    if opt_Ask in sum_End.upper():
+        print(' You win!!!')
+        gam_Ask = str(input('Want to retry (Y/N)? '))
+        gam_Win =+ 1
+    else:
+        print(' Game over.')
+        break
+print(f'You won {gam_Win} games.')
