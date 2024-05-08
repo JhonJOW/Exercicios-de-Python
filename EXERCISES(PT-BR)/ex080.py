38b8aeb3b86549fc44531e47c24554648e732b1a
Gustavo Simonelli Franco <g19sfranco@gmail.com>
2024-05-08 17:21:07 -0300

    Update ex080.py

--- EXERCISES(PT-BR)/ex080.py
+++ EXERCISES(PT-BR)/ex080.py
@@ -1,1 +1,13 @@
-num_List = 
+num_List = []
+for j in range(0, 5):
+    num_Digit = int(input("Type a value: "))
+    if j == 0 or num_Digit > num_List[-1]:
+        num_List.append(num_Digit)
+    else:
+        list_Position = 0
+        while list_Position < len(num_List):
+            if num_Digit <= num_List[list_Position]:
+                num_List.insert(list_Position, num_Digit)
+                break
+            list_Position += 1
+print(f"The typed values were {num_List}.")
