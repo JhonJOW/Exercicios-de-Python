52af1cdad2f6590711c8227d18a5e62c7f1dccce
Gustavo Simonelli Franco <g19sfranco@gmail.com>
2024-07-22 09:47:46 -0300

    Update ex080.py

--- EXERCISES(PT-BR)/ex080.py
+++ EXERCISES(PT-BR)/ex080.py
@@ -3,11 +3,14 @@ for j in range(0, 5):
     num_Digit = int(input("Type a value: "))
     if j == 0 or num_Digit > num_List[-1]:
         num_List.append(num_Digit)
+        print(f"The value {num_Digit} was placed in {j} position.")
     else:
         list_Position = 0
         while list_Position < len(num_List):
             if num_Digit <= num_List[list_Position]:
                 num_List.insert(list_Position, num_Digit)
+                print(f"The value {num_Digit} was placed in {list_Position} position.")
                 break
             list_Position += 1
+            
 print(f"The typed values were {num_List}.")
